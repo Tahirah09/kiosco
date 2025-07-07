d82e72e73412ea9e2a695e11831cf8ab6f707ec6
Tahirah09 <danihussagh@ctpantoniomarte.edu.ar>
2025-07-07 17:05:43 -0300

    comentario categoria

--- apps/despensa/models.py
+++ apps/despensa/models.py
@@ -11,7 +11,7 @@ class Producto(models.Model):
   def __str__(self):
     return f"{self.nombre}.{self.descripcion}"
 
-
+#models categoria
 class Categoria(models.Model):
   nombre = models.CharField(max_length=100)
   descripcion = models.CharField(max_length=100)
